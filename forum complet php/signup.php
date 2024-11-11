63e2b3078eef74a77c56ac824fbd7e335f811937
angelique <zeleokakua@gmail.com>
2024-11-11 18:45:10 +0100

    Add files via upload

---
+++ forum complet php/signup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+ <?php include 'includes/head.php';?>
+
+<body>
+    <br>
+    <br>
+<form class="container">
+  <div class="mb-3">
+    <label for="Pseudo" class="form-label">Pseudo</label>
+    <input type="text" class="form-control" >
+  </div>
+  <div class="mb-3">
+    <label for="Nom" class="form-label">Nom</label>
+    <input type="text" class="form-control" >
+  </div>
+  <div class="mb-3">
+    <label for="Prenom" class="form-label">Prénom</label>
+    <input type="text" class="form-control" >
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control">
+  </div>
+  <button type="submit" class="btn btn-primary">S'inscrire</button>
+</form>
+    
+</body>
+</html>
