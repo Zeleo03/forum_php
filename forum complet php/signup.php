f576d8eede958fd7ff0cbab777cca6e5103ec081
angelique <zeleokakua@gmail.com>
2024-11-11 22:22:53 +0100

    Add files via upload

--- forum complet php/signup.php
+++ forum complet php/signup.php
@@ -5,24 +5,24 @@
 <body>
     <br>
     <br>
-<form class="container">
+<form class="container" method="POST">
   <div class="mb-3">
     <label for="Pseudo" class="form-label">Pseudo</label>
-    <input type="text" class="form-control" >
+    <input type="text" class="form-control" name="pseudo">
   </div>
   <div class="mb-3">
     <label for="Nom" class="form-label">Nom</label>
-    <input type="text" class="form-control" >
+    <input type="text" class="form-control" name="lastname">
   </div>
   <div class="mb-3">
     <label for="Prenom" class="form-label">Prénom</label>
-    <input type="text" class="form-control" >
+    <input type="text" class="form-control" name="firstname">
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control">
+    <input type="password" class="form-control" name="password">
   </div>
-  <button type="submit" class="btn btn-primary">S'inscrire</button>
+  <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
 </form>
     
 </body>
